217d3a88044a4b8564a2d9a1b8afe7ba67ee95df
Bishop <dommennic@gmail.com>
2020-10-02 19:54:15 +0300

    New Data types.

--- generator/models.py
+++ generator/models.py
@@ -10,83 +10,60 @@ class DataSchema(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
+DATA_TYPES = (
+    (0, 'Integer'),
+    (1, 'Job'),
+    (2, 'Email'),
+    (3, 'Domain'),
+    (4, 'Company')
+)
+
+
 class Column(models.Model):
     name = models.CharField(max_length=100, null=False)
     order = models.IntegerField()
     schema = models.ForeignKey(DataSchema, on_delete=models.CASCADE)
-
-
-# Data types.
-
-class Integer(models.Model):
-    range_from = models.IntegerField()
-    range_to = models.IntegerField()
-    column = models.OneToOneField(Column, on_delete=models.CASCADE)
+    data_type = models.IntegerField(choices=DATA_TYPES)
+    value_range_from = models.IntegerField()
+    value_range_to = models.IntegerField()
 
     def generate_fake_value(self):
-        return random.randint(self.range_from, self.range_to)
+        # Integer.
+        if self.data_type == 0:
+            return random.randint(self.value_range_from, self.value_range_to)
 
+        # Job.
+        if self.data_type == 1:
+            return random.choice([
+                'Project Manager',
+                'Lead Developer',
+                'QA',
+                'SEO',
+            ])
 
-class Job(models.Model):
-    column = models.OneToOneField(Column, on_delete=models.CASCADE)
+        # Email.
+        if self.data_type == 2:
+            return random.choice([
+                'fake@gmail.com',
+                'fake.fake@gmail.com',
+                'fake.fake.fake@gmail.com',
+                'funny.joke@gmail.com',
+            ])
 
-    @staticmethod
-    def generate_fake_value():
-        return random.choice([
-            'Project Manager',
-            'Lead Developer',
-            'QA',
-            'SEO',
-        ])
+        # Domain.
+        if self.data_type == 3:
+            return random.choice([
+                'domain.com',
+                'something.com',
+                'fake-domain.com',
+                'another-one.com',
+            ])
 
-
-class Email(models.Model):
-    column = models.OneToOneField(Column, on_delete=models.CASCADE)
-
-    @staticmethod
-    def generate_fake_value():
-        return random.choice([
-            'fake@gmail.com',
-            'fake.fake@gmail.com',
-            'fake.fake.fake@gmail.com',
-            'funny.joke@gmail.com',
-        ])
-
-
-class DomainName(models.Model):
-    column = models.OneToOneField(Column, on_delete=models.CASCADE)
-
-    @staticmethod
-    def generate_fake_value():
-        return random.choice([
-            'domain.com',
-            'something.com',
-            'fake-domain.com',
-            'another-one.com',
-        ])
-
-
-class PhoneNumber(models.Model):
-    column = models.OneToOneField(Column, on_delete=models.CASCADE)
-
-    @staticmethod
-    def generate_fake_value():
-        return random.choice([
-            '+380-311-8555-52',
-            '+380-635-5559-18',
-            '+380-109-6555-50',
-            '+380-261-6555-63',
-        ])
-
-
-class CompanyName(models.Model):
-    column = models.OneToOneField(Column, on_delete=models.CASCADE)
-
-    @staticmethod
-    def generate_fake_value():
-        return random.choice([
-            'Fake Company',
-            'Not Real Services',
-            'Something Company',
-            'Not A Company',
-        ])
+        # Company.
+        if self.data_type == 4:
+            return random.choice([
+                'Fake Company',
+                'Not Real Services',
+                'Something Company',
+                'Not A Company',
+            ])
